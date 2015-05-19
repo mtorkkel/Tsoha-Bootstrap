8cd1d40b804eca09f3166bfd3cd0654a4db3a1de
Torkkeli Mari A <mtorkkel@da1-cs-bk107-07.helsinki.fi>
2015-05-19 12:41:33 +0300

    korjaus

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -2,5 +2,6 @@
 -- Opettaja-taulun testidata
 INSERT INTO Opettaja (nimi, salasana) VALUES ('Ope1', 'Ope1salas'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
 INSERT INTO Opettaja (nimi, salasana) VALUES ('Ope2', 'Ope2salas');
+INSERT INTO Opettaja (nimi, salasana) VALUES ('Ope3', 'Ope3salas');
 -- Sovellus-taulun testidata
 INSERT INTO Sovellus (nimi, url, lyhytkuvaus, lisatty) VALUES ('Kahoot', 'getkahoot.com', 'Pelillinen kyselytyökalu', NOW());
