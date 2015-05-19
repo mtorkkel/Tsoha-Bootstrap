1c212aa44ebb705f992f5d94b209182f09e7ce16
Torkkeli Mari A <mtorkkel@da1-cs-bk107-07.helsinki.fi>
2015-05-19 15:08:00 +0300

    paivitys

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -5,3 +5,5 @@ INSERT INTO Opettaja (nimi, salasana) VALUES ('Ope2', 'Ope2salas');
 INSERT INTO Opettaja (nimi, salasana) VALUES ('Ope3', 'Ope3salas');
 -- Sovellus-taulun testidata
 INSERT INTO Sovellus (nimi, url, lyhytkuvaus, lisatty) VALUES ('Kahoot', 'getkahoot.com', 'Pelillinen kyselytyökalu', NOW());
+
+INSERT INTO Ryhma (nimi, selitys) VALUES ('Mobiilisovellus', 'Sopii käytettäväksi kännykällä.');
