d09dfa76241493bbeb62e37320a2c5fd6afcc67d
Torkkeli Mari A <mtorkkel@da1-cs-bk107-07.helsinki.fi>
2015-05-19 12:38:23 +0300

    testidatan korjaus

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,6 +1,6 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon  
 -- Opettaja-taulun testidata
 INSERT INTO Opettaja (nimi, salasana) VALUES ('Ope1', 'Ope1salas'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
-INSERT INTO Player (name, password) VALUES ('Ope2', 'Ope2salas');
+INSERT INTO Opettaja (nimi, salasana) VALUES ('Ope2', 'Ope2salas');
 -- Sovellus-taulun testidata
 INSERT INTO Sovellus (nimi, url, lyhytkuvaus, lisatty) VALUES ('Kahoot', 'getkahoot.com', 'Pelillinen kyselytyökalu', NOW());
