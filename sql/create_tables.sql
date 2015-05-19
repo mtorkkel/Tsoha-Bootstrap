beff64e1d7b70fd32bf57f8fb56f7d0071aca012
Torkkeli Mari A <mtorkkel@da1-cs-bk107-07.helsinki.fi>
2015-05-19 12:26:23 +0300

    taulut

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,16 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Opettaja(
+  id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
+  nimi varchar(50) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
+  salasana varchar(50) NOT NULL
+);
+
+CREATE TABLE Sovellus(
+  id SERIAL PRIMARY KEY,
+  sovellus_id INTEGER REFERENCES Opettaja(id), -- Viiteavain Opettaja-tauluun
+  nimi varchar(50) NOT NULL,
+  url varchar(400),
+  lyhytkuvaus varchar(400),
+  status varchar(50),
+  lisatty DATE
+  );
--- sql/drop_tables.sql
+++ sql/drop_tables.sql
@@ -1,1 +1,3 @@
 -- Lisää DROP TABLE lauseet tähän tiedostoon
+DROP TABLE IF EXISTS Sovellus CASCADE; -- Muista IF EXISTS ja CASCADE parametrit!
+DROP TABLE IF EXISTS Opettaja CASCADE;
